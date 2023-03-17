9011d4cbc3735d9f41c1d09a70180dc03c6d3481
user <ujjalroy7862@gmail.com>
2023-03-18 01:19:09 +0600

    done the routing for go to the Add new page

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,17 @@ import React from 'react';
 import Cards from './components/Cards';
 import Header from './components/Header';
 
+import { Routes, Route } from 'react-router-dom';
+import AddMovie from './components/AddMovie';
+
 const App = () => {
     return (
         <>
             <Header />
-            <Cards />
+            <Routes>
+                <Route path="/" element={<Cards />} />
+                <Route exact path="/add-movie" element={<AddMovie />} />
+            </Routes>
         </>
     );
 };
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import { GrAdd } from 'react-icons/gr';
 import { Button } from '@mui/material';
+import { Link } from 'react-router-dom';
+
 const Header = () => {
     return (
         <div className="text-3xl text-red-500 font-bold [-3 border-b-2 border-gray-400 flex items-center justify-between p-4">
-            <span>
+            <Link to="/">
                 Movie <span className="text-white uppercase">Hub</span>{' '}
-            </span>
-
-            <Button variant="contained" color="inherit" startIcon={<GrAdd />}>
-                <span className="text-red-500 flex items-center cursor-pointer font-bold">Add New</span>
-            </Button>
+            </Link>
+            <Link to="/add-movie">
+                <Button variant="contained" color="inherit" startIcon={<GrAdd />}>
+                    <span className="text-red-500 flex items-center cursor-pointer font-bold">Add New</span>
+                </Button>
+            </Link>
         </div>
     );
 };

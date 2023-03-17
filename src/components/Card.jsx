b18f479ca35cae43b0fc722f5295bc43d3bd79cc
user <ujjalroy7862@gmail.com>
2023-03-18 02:55:29 +0600

    fetching data from the data base is done

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,18 +2,19 @@ import { Rating } from '@mui/material';
 import React from 'react';
 
 const Card = (props) => {
-    const { name, rating, imgUrl, year } = props.data;
+    const { title, img, year } = props.data;
+
     return (
         <div className="rounded-md shadow-lg bg-slate-500 p-2 transition-all">
-            <img src={imgUrl} alt={name} className="w-[250px] h-[350px] hover:scale-110 transition-all rounded-2xl" />
+            <img src={img} alt={title} className="w-[250px] h-[350px] hover:scale-110 transition-all rounded-2xl" />
 
             {/* information */}
             <div className="mt-5">
                 {' '}
-                <h1 className="text-2xl font-bold">Name: {name}</h1>
+                <h1 className="text-2xl font-bold ">Name: {title.slice(0, 10) + '...'}</h1>
                 <h1 className="text-xl font-bold flex items-center gap-4">
                     Rating:
-                    <Rating name="half-rating" defaultValue={rating} precision={0.5} sx={{ marginleft: '10px' }} />
+                    <Rating name="half-rating" defaultValue={5} precision={0.5} sx={{ marginleft: '10px' }} />
                 </h1>
                 <h1 className="text-xl font-bold">Year: {year}</h1>
             </div>
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,23 +1,38 @@
+import { getDocs } from 'firebase/firestore';
 import React, { useEffect, useState } from 'react';
+import { Dna } from 'react-loader-spinner';
 import { movieData } from '../dummtData';
 import Card from './Card';
+import { moviesRef } from './firebase/firebase';
 
 export default function Cards() {
-    const [data, setData] = useState(movieData);
-
+    const [data, setData] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
     useEffect(() => {
         const getData = async () => {
-            try {
-            } catch (error) {}
+            setIsLoading(true);
+            const data = await getDocs(moviesRef);
+            data.forEach((doc) => {
+                setData((pS) => [...pS, doc.data()]);
+            });
+            setIsLoading(false);
         };
+        getData();
     }, []);
     return (
         <>
-            <div className="grid grid-cols-4 justify-items-center mt-10 gap-6">
-                {data.map((movie, i) => {
-                    return <Card key={i} data={movie} />;
-                })}
-            </div>
+            {isLoading ? (
+                <div className="flex justify-center items-center h-screen">
+                    <Dna height={100} width="180" color="white" />
+                </div>
+            ) : (
+                <div className="grid grid-cols-4 justify-items-center mt-10 gap-6">
+                    {data &&
+                        data.map((movie, i) => {
+                            return <Card key={i} data={movie} />;
+                        })}
+                </div>
+            )}
         </>
     );
 }
